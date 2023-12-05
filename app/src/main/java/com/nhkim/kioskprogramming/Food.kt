8d81f6308e355142ea6f3878cc302ccb62aa39d2
Ohleesang <jammmmed@gmail.com>
2023-12-05 18:07:34 +0900

    클래스 다이어그램 보고 클래스 뼈대 작성

---
+++ app/src/main/java/com/nhkim/kioskprogramming/Food.kt
@@ -0,0 +1,8 @@
+package com.nhkim.kioskprogramming
+
+open class Food {
+    var name :String =""
+    var price :Int =0
+
+    open fun displayInfo(){}
+}
---
+++ app/src/main/java/com/nhkim/kioskprogramming/Kiosk.kt
@@ -0,0 +1,10 @@
+package com.nhkim.kioskprogramming
+
+class Kiosk {
+    var input :String? = null
+    var foods :MutableList<Food> = mutableListOf<Food>()
+    fun mainMenu(){}
+    fun orderMenu(){}
+    fun cart(){}
+
+}
