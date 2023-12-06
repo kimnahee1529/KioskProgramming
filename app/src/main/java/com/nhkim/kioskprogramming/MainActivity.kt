0f248bd1eb1155ec368e02cd8252111f2e32d7a3
Ohleesang <jammmmed@gmail.com>
2023-12-06 13:00:18 +0900

    mainMenu() 구현

--- app/src/main/java/com/nhkim/kioskprogramming/MainActivity.kt
+++ app/src/main/java/com/nhkim/kioskprogramming/MainActivity.kt
@@ -2,7 +2,6 @@ package com.nhkim.kioskprogramming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
