a5e77c402af4b71ad88069ee7bd214df3e8313ed
Ohleesang <jammmmed@gmail.com>
2023-12-06 12:58:46 +0900

    mainMenu() 구현

--- app/src/main/java/com/nhkim/kioskprogramming/MainActivity.kt
+++ app/src/main/java/com/nhkim/kioskprogramming/MainActivity.kt
@@ -2,13 +2,17 @@ package com.nhkim.kioskprogramming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //fun main() 부분
-
     }
+}
+//fun main() 부분
+fun main(){
+    var kiosk = Kiosk()
+    kiosk.mainMenu()
 }
