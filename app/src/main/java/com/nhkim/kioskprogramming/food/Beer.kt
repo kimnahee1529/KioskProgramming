2b98b96f3064cf52dc7a2630949359257d70f475
nhkim <nhkim1529@gmail.com>
2023-12-07 20:21:04 +0900

    Feat: Beer 클래스 생성

--- app/src/main/java/com/nhkim/kioskprogramming/food/Beer.kt
+++ app/src/main/java/com/nhkim/kioskprogramming/food/Beer.kt
@@ -1,10 +1,15 @@
 package com.nhkim.kioskprogramming.food
 
-class Beer: Food() {
+class Beer(menuItems: List<MenuItem>): Food() {
     var size :Int =0
     var type :String =""
-    override fun displayInfo(){}
 
+    override fun displayInfo(category: String, menuItems: List<MenuItem>) {
+        super.displayInfo("Beer", menuItems)
+    }
+    override fun selectMenu(menuItems: List<MenuItem>) {
+        super.selectMenu(menuItems)
+    }
     fun selectSize(){}
     fun selectType(){}
 }
