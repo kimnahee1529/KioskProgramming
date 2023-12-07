569cc5b8cc08506df75e1cede3cb62bcea3c260c
chris <chris@trulite.co>
2023-12-07 21:00:37 +0900

    Kiosk.kt의 displayMainMenu 메소드 수정

--- app/src/main/java/com/nhkim/kioskprogramming/Kiosk.kt
+++ app/src/main/java/com/nhkim/kioskprogramming/Kiosk.kt
@@ -30,11 +30,11 @@ fun main() {
 
 fun displayMainMenu() {
     println("[ SHAKESHACK MENU ]")
-    println("1. Burgers")
-    println("2. Frozen Custard")
-    println("3. Drinks")
-    println("4. Beer")
-    println("0. 종료")
+    println("1. Burgers         | 앵거스 비프 통살을 다져만든 버거")
+    println("2. Frozen Custard  | 매장에서 신선하게 만드는 아이스크림")
+    println("3. Drinks          | 매장에서 직접 만드는 음료")
+    println("4. Beer            | 뉴욕 브루클린 브루어리에서 양조한 맥주")
+    println("0. 종료             | 프로그램 종료")
     print("> ")
 }
 
