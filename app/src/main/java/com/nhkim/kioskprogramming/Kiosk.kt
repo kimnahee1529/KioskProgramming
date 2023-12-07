7ff99c4ff7080f4fbe3d6b29d67fdc35c2ff7eb8
Ohleesang <jammmmed@gmail.com>
2023-12-07 10:41:24 +0900

    Feat : add initMenu() change mainMenu() change orderMenu() add burgerList,iceCremList,drinkList,beerList
    initMenu() : 메뉴판 문자열을 등록하는 함수 추가 그에따른 문자열을 출력하기 위해 각 Food 자식 클래스 (Burger,IceCream,Drink,Beer) 의 mutableList 를 만들고, 인스턴스들을 생성
    
    mainMenu() : 메뉴판 출력부분이 반복되어야하므로 println(line)을 while문 안에 변경
    
    orderMenu()->orderMenu(i: Int) : input의 입력된 값 을 받아처리 해야하므로 파라미터 추가
    
    orderMenu(i: Int) :
    매개변수 i 에 해당되는 번호에 상세한 메뉴판을 출력, 0 이 입력되면 함수 를 return
    
    burgerList,iceCremList,drinkList,beerList : 메뉴에 등록되어진 음식(인스턴스)의 리스트.
    
    Fixes : #1 initMenu() : 함수내에 인스턴스 생성에 대한 값을 넣어야함(비어있는 문자열 입력 해야됨)

--- app/src/main/java/com/nhkim/kioskprogramming/Kiosk.kt
+++ app/src/main/java/com/nhkim/kioskprogramming/Kiosk.kt
@@ -1,12 +1,49 @@
 package com.nhkim.kioskprogramming
 
-import com.nhkim.kioskprogramming.food.Food
+import com.nhkim.kioskprogramming.food.*
 
 class Kiosk {
     var input :Int? = null
     var foods :MutableList<Food> = mutableListOf<Food>()
+
+    var burgerList = mutableListOf<Food>()
+    var iceCreamList = mutableListOf<Food>()
+    var drinkList = mutableListOf<Food>()
+    var beerList = mutableListOf<Food>()
+
+
+    //메뉴 초기화
+    //추후 음식 주문할때 정보들을 저장
+    fun initMenu(){
+
+        burgerList.add(Burger("ShackBurger","토마토, 양상추, 쉑소스가 토핑된 치즈버거",6.9))
+        burgerList.add(Burger("","",0.0))
+        burgerList.add(Burger("","",0.0))
+        burgerList.add(Burger("","",0.0))
+        burgerList.add(Burger("","",0.0))
+
+        iceCreamList.add(IceCream("","",0.0))
+        iceCreamList.add(IceCream("","",0.0))
+        iceCreamList.add(IceCream("","",0.0))
+        iceCreamList.add(IceCream("","",0.0))
+        iceCreamList.add(IceCream("","",0.0))
+        iceCreamList.add(IceCream("","",0.0))
+
+        drinkList.add(Drink("","",0.0))
+        drinkList.add(Drink("","",0.0))
+        drinkList.add(Drink("","",0.0))
+        drinkList.add(Drink("","",0.0))
+        drinkList.add(Drink("","",0.0))
+        drinkList.add(Drink("","",0.0))
+
+        beerList.add(Beer("","",0.0))
+        beerList.add(Beer("","",0.0))
+
+    }
     fun mainMenu(){
 
+        // 메뉴판 초기화
+        initMenu()
         // 메뉴판 출력
         val line ="""
             "SHAKESHACK BURGER 에 오신걸 환영합니다."
@@ -20,15 +57,19 @@ class Kiosk {
             0. 종료            | 프로그램 종료
            
         """.trimIndent()
-        println(line)
 
         //값을 입력
         while(true) {
             try {
+
+
+                println(line)
+                //만약 foods에 값이 있다면 order menu 출력
+
                 input = readLine()?.toInt()
                 when(input){
                     0 -> break
-                    in 1..4 -> orderMenu()
+                    in 1..4 -> orderMenu(input!!)
                 }
                 println(input)
             } catch (e: Exception) {
@@ -36,9 +77,45 @@ class Kiosk {
             }
         }
     }
-    fun orderMenu(){
-        println("OrderMenu 생성")
-        println()
+    fun orderMenu(i: Int){
+        when(i){
+            1 -> {
+                println("[ Burgers MENU ]")
+                for(i in burgerList.indices){
+                    print("${i+1}. ")
+                    burgerList[i].displayInfo()
+                }
+            }
+            2 -> {
+                println("[ Frozen Custard MENU ]")
+                for(i in iceCreamList.indices){
+                    print("${i+1}. ")
+                    iceCreamList[i].displayInfo()
+                }
+            }
+            3 -> {
+                println("[ Drinks MENU ]")
+                for(i in drinkList.indices){
+                    print("${i+1}. ")
+                    drinkList[i].displayInfo()
+                }
+            }
+            4 -> {
+                println("[ Beer MENU ]")
+                for(i in beerList.indices){
+                    print("${i+1}. ")
+                    beerList[i].displayInfo()
+                }
+            }
+        }
+        println("0. 뒤로가기      | 뒤로가기")
+        //입력부분
+        while(true){
+            input = readLine()?.toInt()
+            when(input){
+                0 -> break
+            }
+        }
 
     }
     fun cart(){}
