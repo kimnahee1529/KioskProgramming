7b1ab7f71d2e12a3d21f497f50c749c09c12152a
Ohleesang <jammmmed@gmail.com>
2023-12-07 10:24:27 +0900

    Feat : Change property Constructor displayInfo()
    IceCream 클래스 프로퍼티,생성자 변경
    override 의 필요성이 없어서 disPlayInfo() 주석 처리

---
+++ app/src/main/java/com/nhkim/kioskprogramming/food/IceCream.kt
@@ -0,0 +1,11 @@
+package com.nhkim.kioskprogramming.food
+
+class IceCream(name :String,detail :String,price:Double): Food(name,detail,price) {
+
+    var size :Int =0
+    var flavor :String =""
+//    override fun displayInfo(){}
+
+    fun selectSize(){}
+    fun selectFlavor(){}
+}
