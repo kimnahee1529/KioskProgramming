1de2c4a3a3e317c040fcfa530f22e6f87c6d2ce4
nhkim <nhkim1529@gmail.com>
2023-12-07 20:21:13 +0900

    Feat: Burger 클래스 생성

--- app/src/main/java/com/nhkim/kioskprogramming/food/Burger.kt
+++ app/src/main/java/com/nhkim/kioskprogramming/food/Burger.kt
@@ -1,12 +1,14 @@
 package com.nhkim.kioskprogramming.food
 
-class Burger: Food() {
-    override fun displayInfo(){}
+import com.nhkim.kioskprogramming.Kiosk
 
+class Burger(menuItems: List<MenuItem>): Food() {
+    override fun displayInfo(category: String, menuItems: List<MenuItem>) {
+        super.displayInfo("Burgers", menuItems)
+    }
+
+    override fun selectMenu(menuItems: List<MenuItem>) {
+        super.selectMenu(menuItems)
+    }
     fun addTopping():Int = 0
 }
-
-
-
-
-
