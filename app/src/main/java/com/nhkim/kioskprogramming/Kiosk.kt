e02a6b0376d0ba4288ca411757b26220403fdda0
Ohleesang <jammmmed@gmail.com>
2023-12-07 10:57:44 +0900

    Style : Add instance by burgerList,iceCreamList,drinkList,beerList
    값이 비어있는 인스턴스들에 매개변수 값(이름,상세,값) 을 입력
    
    Resolves : #1

--- app/src/main/java/com/nhkim/kioskprogramming/Kiosk.kt
+++ app/src/main/java/com/nhkim/kioskprogramming/Kiosk.kt
@@ -17,27 +17,27 @@ class Kiosk {
     fun initMenu(){
 
         burgerList.add(Burger("ShackBurger","토마토, 양상추, 쉑소스가 토핑된 치즈버거",6.9))
-        burgerList.add(Burger("","",0.0))
-        burgerList.add(Burger("","",0.0))
-        burgerList.add(Burger("","",0.0))
-        burgerList.add(Burger("","",0.0))
+        burgerList.add(Burger("SmokeShack","베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거",8.9))
+        burgerList.add(Burger("Shroom Burger","몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거",9.4))
+        burgerList.add(Burger("Cheeseburger","포테이토 번과 비프패티, 치즈가 토핑된 치즈버거",6.9))
+        burgerList.add(Burger("Hamburger","비프패티를 기반으로 야채가 들어간 기본버거",5.4))
 
-        iceCreamList.add(IceCream("","",0.0))
-        iceCreamList.add(IceCream("","",0.0))
-        iceCreamList.add(IceCream("","",0.0))
-        iceCreamList.add(IceCream("","",0.0))
-        iceCreamList.add(IceCream("","",0.0))
-        iceCreamList.add(IceCream("","",0.0))
+        iceCreamList.add(IceCream("Shakes","바닐라, 초콜렛, 솔티드 카라멜, 블랙 & 화이트, 스트로베리, 피넛버터, 커피",5.9))
+        iceCreamList.add(IceCream("Shake of the Week","특별한 커스터드 플레이버",6.5))
+        iceCreamList.add(IceCream("Red Bean Shake","신선한 커스터드와 함께 우유와 레드빈이 블렌딩된 시즈널 쉐이크",6.5))
+        iceCreamList.add(IceCream("Floats","루트 비어, 퍼플 카우, 크림사클",5.9))
+        iceCreamList.add(IceCream("Cups & Cones","바닐라, 초콜렛, Flavor of the Week",4.9))
+        iceCreamList.add(IceCream("Concretes","쉐이크쉑의 쫀득한 커스터드와 다양한 믹스-인의 조합",5.9))
 
-        drinkList.add(Drink("","",0.0))
-        drinkList.add(Drink("","",0.0))
-        drinkList.add(Drink("","",0.0))
-        drinkList.add(Drink("","",0.0))
-        drinkList.add(Drink("","",0.0))
-        drinkList.add(Drink("","",0.0))
+        drinkList.add(Drink("Shack-made Lemonade","매장에서 직접 만드는 상큼한 레몬에이드(오리지날/시즈널)",3.9))
+        drinkList.add(Drink("Fresh Brewed Iced Tea","직접 유기농 홍차를 우려낸 아이스티",3.4))
+        drinkList.add(Drink("Fifty/Fifty","레몬에이드와 아이스티의 만남",3.5))
+        drinkList.add(Drink("Fountain Soda","코카콜라, 코카콜라제로,스프라이트, 환타 오렌지, 환타 그레이프",2.7))
+        drinkList.add(Drink("Abita Root Beer","청량감 있는 독특한 미국식 무알콜 탄산음료",4.4))
+        drinkList.add(Drink("Bottled Water","지리산 암반대수층으로 만든 프리미엄 생수",1.0))
 
-        beerList.add(Beer("","",0.0))
-        beerList.add(Beer("","",0.0))
+        beerList.add(Beer("ShackMeister Ale","쉐이크쉑 버거를 위해 뉴욕 브루클린 브루어리에서 특별히 양조한 에일 맥주",9.8))
+        beerList.add(Beer("Magie Brewing Co.","Pole Ale, Draft / Porter , Draft / Wheat,Draft",6.8))
 
     }
     fun mainMenu(){
