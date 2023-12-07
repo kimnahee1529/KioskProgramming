1a694372453f013ab832615c69aa38e671c7b33e
nhkim <nhkim1529@gmail.com>
2023-12-07 20:22:04 +0900

    Feat: IceCream 클래스 생성

--- app/src/main/java/com/nhkim/kioskprogramming/food/IceCream.kt
+++ app/src/main/java/com/nhkim/kioskprogramming/food/IceCream.kt
@@ -1,11 +1,20 @@
 package com.nhkim.kioskprogramming.food
 
-class IceCream: Food() {
+class IceCream(menuItems: List<MenuItem>): Food() {
 
     var size :Int =0
     var flavor :String =""
-    override fun displayInfo(){}
+
+    override fun displayInfo(category: String, menuItems: List<MenuItem>) {
+        super.displayInfo("IceCream", menuItems)
+    }
+
+    override fun selectMenu(menuItems: List<MenuItem>) {
+        super.selectMenu(menuItems)
+    }
 
     fun selectSize(){}
-    fun selectFlavor(){}
+    fun selectFlavor(){
+
+    }
 }
