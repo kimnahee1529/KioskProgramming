92cef618af3db3a0df264b56c78b6893c25fc925
nhkim <nhkim1529@gmail.com>
2023-12-07 20:21:20 +0900

    Feat: Drink 클래스 생성

--- app/src/main/java/com/nhkim/kioskprogramming/food/Drink.kt
+++ app/src/main/java/com/nhkim/kioskprogramming/food/Drink.kt
@@ -1,9 +1,14 @@
 package com.nhkim.kioskprogramming.food
 
-class Drink: Food() {
+class Drink(menuItems: List<MenuItem>): Food() {
 
     var size :String =""
-    override fun displayInfo(){}
+    override fun displayInfo(category: String, menuItems: List<MenuItem>) {
+        super.displayInfo("IceCream", menuItems)
+    }
+    override fun selectMenu(menuItems: List<MenuItem>) {
+        super.selectMenu(menuItems)
+    }
 
     fun addIce(){}
     fun selectSize(){}
